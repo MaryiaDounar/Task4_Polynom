3021eb50c9fe052d26f11b46439ff6d832ee7b83
maryia_dounar <maryia_dounar@epam.com>
2015-07-30 17:21:08 +0300

    Add Test

---
+++ Polynom/Polynom.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polynomial
+{
+    public class Polynom :ICloneable, IEquatable<Polynom>
+    {
+        private double[] coefficient;
+        private int degree;
+
+        public Polynom(params double[] coefficient)
+        {
+            this.degree = coefficient.Length - 1;
+            this.coefficient = (double[])coefficient.Clone();
+        }
+
+        public double this[int i]
+        {
+            get
+            {
+                return this.coefficient[i];
+            }
+        }
+
+       
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i=0; i<=degree-1; i++)
+            {
+                stringBuilder.AppendFormat("{0}x^{1}+", coefficient[i], i);
+            }
+            stringBuilder.AppendFormat("{0}x^{1}", coefficient[degree], degree);
+            return stringBuilder.ToString();
+        }
+
+        public bool Equals(Polynom other)
+        {
+            if(ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if(ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            return this.coefficient.Length == other.coefficient.Length && this.coefficient.SequenceEqual(other.coefficient);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if(ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+
+            Polynom polynom = obj as Polynom;
+            if(polynom == null)
+            {
+                return false;
+            }
+
+            return Equals(polynom);
+        }
+
+        public static bool operator ==(Polynom polynom1, Polynom polynom2)
+        {
+            if (ReferenceEquals(polynom1, null) || ReferenceEquals(polynom2, null))
+            {
+                return false;
+            }
+
+            return polynom1.Equals(polynom2);
+        }
+
+        public static bool operator !=(Polynom polynom1, Polynom polynom2)
+        {
+            if (ReferenceEquals(polynom1, null) || ReferenceEquals(polynom2, null))
+            {
+                return false;
+            }
+            return !(polynom1.Equals(polynom2));
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public object Clone()
+        {
+            return new Polynom(this.coefficient);
+        }
+
+        public static Polynom operator +(Polynom a, Polynom b)
+        {
+            Polynom greater;
+            Polynom less;
+            if (a.degree > b.degree)
+            {
+                greater = a;
+                less = b;
+            }
+            else
+            {
+                greater = b;
+                less = a;
+            }
+            double[] coeff = new double[greater.degree + 1];
+            for(int i=0; i<=less.degree; i++)
+            {
+                coeff[i] = less.coefficient[i];
+            }
+            for(int i=0; i<=greater.degree; i++)
+            {
+                coeff[i] += greater.coefficient[i];
+            }
+            Polynom c = new Polynom(coeff);
+            return c;
+        }
+
+        public static Polynom operator -(Polynom a)
+        {
+            double[] coeff = new double[a.coefficient.Length];
+            for(int i=0; i<coeff.Length; i++)
+            {
+                coeff[i] = a.coefficient[i] * -1;
+            }
+            return new Polynom(coeff);
+        }
+
+        public static Polynom operator -(Polynom a, Polynom b)
+        {
+            return a + (-b);
+        }
+
+        public static Polynom operator *(Polynom a, Polynom b)
+        {
+            double[] coeff = new double[a.degree + b.degree + 1];
+            for(int i=0; i<=a.degree ; i++)
+            {
+                for(int j=0; j<=b.degree; j++)
+                {
+                    coeff[i + j] += a[i] * b[j]; 
+                }
+            }
+            Polynom c = new Polynom(coeff);
+            return c;
+        }
+
+        public double Calculate(int degree, double[] matrix, double x)
+        {
+            double value = 0;
+            for(int i=0; i<=degree; i++)
+            {
+                x *= matrix[i];
+                value += Math.Pow(x, i);
+            }
+            return value;
+        }
+    }
+}
---
+++ PolynomialTest/PolynomTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Polynomial;
+using LumenWorks.Framework.IO.Csv;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PolynomialTest
+{
+    [TestFixture]
+    public class PolynomTest
+    {
+      // private CsvReader csv = new CsvReader(new StreamReader("Data.csv"), true);
+
+       public double[] coeffPolynom1 = { 1, 2, 3 };
+       private double[] coeffPolynom2 = { 1, 2, 3, 4 };
+       private double[] coeffAdd = { 2, 4, 6, 4 };
+       private double[] coeffSub = { 0, 0, 0, 4 };
+       private double[] coeffMul = { 1, 4, 10, 16, 17, 12 };
+
+
+       [Test]
+       public void Add()
+        {
+            Polynom polynom1 = new Polynom(coeffPolynom1);
+            Polynom polynom2 = new Polynom(coeffPolynom2);
+            Polynom expectedPolynom = new Polynom(coeffAdd);
+            Assert.That(polynom1 + polynom2, Is.EqualTo(expectedPolynom));
+        }
+
+       [Test]
+       public void Sub()
+       {
+           Polynom polynom1 = new Polynom(coeffPolynom1);
+           Polynom polynom2 = new Polynom(coeffPolynom2);
+           Polynom expectedPolynom = new Polynom(coeffSub);
+           Assert.That(polynom2 - polynom1, Is.EqualTo(expectedPolynom));
+       }
+
+        [Test]
+        public void Mul()
+       {
+           Polynom polynom1 = new Polynom(coeffPolynom1);
+           Polynom polynom2 = new Polynom(coeffPolynom2);
+           Polynom expectedPolynom = new Polynom(coeffMul);
+           Assert.That(polynom2 * polynom1, Is.EqualTo(expectedPolynom));
+       }
+
+        [Test]
+        public void Equals()
+        {
+            Polynom polynom1 = new Polynom(coeffPolynom1);
+            Polynom polynom2 = new Polynom(coeffPolynom1);
+            Assert.That(polynom1.Equals(polynom2), Is.True);
+        }
+
+        [Test]
+        public void EqualsOperator()
+        {
+            Polynom polynom1 = new Polynom(coeffPolynom1);
+            Polynom polynom2 = new Polynom(coeffPolynom1);
+            Assert.That(polynom1==polynom2, Is.True);
+        }
+
+        [Test]
+        public void NotEqualsOperator()
+        {
+            Polynom polynom1 = new Polynom(coeffPolynom1);
+            Polynom polynom2 = new Polynom(coeffPolynom2);
+            Assert.That(polynom1 != polynom2, Is.True);
+        }
+
+        [Test]
+        public void Clone()
+        {
+            Polynom polynom1 = new Polynom(coeffPolynom1);
+            Polynom polynom2 = (Polynom) polynom1.Clone();
+            Assert.That(polynom1.Equals(polynom2), Is.True);
+        }
+
+       //private IEnumerable<TestCaseData> GetTestData()
+       //{
+      
+       //        while (csv.ReadNextRecord() != false)
+       //        {
+       //            string[] polynom1String = Regex.Split(csv[0], ",");
+       //            string[] polynom2String = Regex.Split(csv[1], ",");
+       //            string[] polynom3String = Regex.Split(csv[2], ",");
+       //            double[] coeffPolynom1 = GetCoefficient(polynom1String);
+       //            double[] coeffPolynom2 = GetCoefficient(polynom2String);
+       //            double[] coeffResult = GetCoefficient(polynom3String);
+       //            yield return new TestCaseData(new []{ coeffPolynom1, coeffPolynom2, coeffResult });
+       //        }
+       //}
+
+   
+
+       //private double[] GetCoefficient(string[] arrayString)
+       //{
+       //    double[] arrayDouble = new double[arrayString.Length];
+       //    for (int i = 0; i < arrayString.Length; i++)
+       //    {
+       //        arrayDouble[i] = double.Parse(arrayString[i]);
+       //    }
+       //    return arrayDouble;
+       //}
+    }
+}
